47dea93a70688c20658facadca23f2fd3cc9fb80
宋金涛 <mr.songjintao@gmail.com>
2025-11-26 21:51:55 +0800

    viteconfig

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -3,10 +3,26 @@ import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"), 
+      "@": resolve(__dirname, "src"),
     },
   },
+
+
+  root: resolve(__dirname, "src"),
+  base: '/',
+  publicDir: resolve(__dirname, "public"),
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+
 })
