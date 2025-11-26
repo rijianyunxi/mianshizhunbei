72da17224fa32d1f7f8bf9e6246e790e01f6802f
宋金涛 <mr.songjintao@gmail.com>
2025-11-26 21:56:04 +0800

    分包

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -12,15 +12,20 @@ export default defineConfig({
   },
 
 
-  root: resolve(__dirname, "src"),
+  root: resolve(__dirname),
   base: '/',
   publicDir: resolve(__dirname, "public"),
   build: {
     outDir: resolve(__dirname, "dist"),
     assetsDir: "assets",
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
+      output: {
+        // 自定义手动分包逻辑
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
       },
     },
   },
