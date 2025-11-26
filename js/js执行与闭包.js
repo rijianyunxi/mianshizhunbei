6f11610f8dc4d76f24baee2db63bc6e17abd0dd2
宋金涛 <mr.songjintao@gmail.com>
2025-11-26 16:02:17 +0800

    项目亮点怎么想

--- js/js执行与闭包.js
+++ js/js执行与闭包.js
@@ -1,16 +1,18 @@
+var a = 99;
 let obj = {
     a:1,
-    b:function(){
-        
-        setTimeout((function(){
-            console.log(this.a)
-        }),3000)
+    b:()=>{
+        console.log(this.a)
+        // setTimeout((function(){
+        //     console.log(this.a)
+        // }),1000)
     },
     c(){
         console.log(this.a)
     }
 }
 
+console.log(obj.b());
 
 
 
--- react/src/views/demo/virtualListDemo/VirtualListDemo.tsx
+++ react/src/views/demo/virtualListDemo/VirtualListDemo.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useRef, useEffect } from "react";
+
 import VirtualList from "@/components/VirtualList";
 
 interface User {
@@ -11,38 +12,47 @@ const users: User[] = Array.from({ length: 1000 }, (_, i) => ({
   name: `用户 ${i}`,
 }));
 
-const test = (
-  <>
-    <div
-      style={{
-        height: 300,
-        width: "100%",
-        border: "1px solid #ccc",
-        overflowY: "auto",
-        position: "relative",
-      }}
-    >
-      <p
-        style={{
-          height: 50,
-          backgroundColor: "#f0f0f0",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          transform: "translateY(500px)",
-        }}
-      >
-        123
-      </p>
-    </div>
-  </>
-);
-
 const VirtualListDemo: React.FC = () => {
+  const testRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      testRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 1000);
+  }, []);
   return (
     <div>
-      <h1 style={{ margin: "32px",fontSize: "24px" }}>虚拟列表</h1>
-      {test}
+      <h1 style={{ margin: "32px", fontSize: "24px" }}>scrollIntoView</h1>
+
+      <div
+        style={{
+          height: 300,
+          width: "100%",
+          border: "1px solid #ccc",
+          overflowY: "auto",
+          position: "relative",
+        }}
+      >
+        <p
+          ref={testRef}
+          style={{
+            height: 50,
+            width: "100%",
+            textAlign: "center",
+            backgroundColor: "#f0f0f0",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            transform: "translateY(500px)",
+          }}
+        >
+          123
+        </p>
+      </div>
+
+      <hr />
+      <h1 style={{ margin: "32px", fontSize: "24px" }}>虚拟列表</h1>
+
       <VirtualList
         data={users}
         itemHeight={50}
