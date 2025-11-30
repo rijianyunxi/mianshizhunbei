2d2f3816ffa31c8e13661cc76bde1a62052cd15a
宋金涛 <mr.songjintao@gmail.com>
2025-11-30 23:05:28 +0800

    泛型

--- webpack/src/main.ts
+++ webpack/src/main.ts
@@ -5,3 +5,6 @@ import '@/assets/reset.css'
 const log = new Log();
 
 log.info<string>("hello webpak,i'm ts loader")
+
+
+log.danger<Error>(new Error("1111"))
--- webpack/src/utils/Log.ts
+++ webpack/src/utils/Log.ts
@@ -1,10 +1,17 @@
 type NoErrorType<T> = [T] extends [Error] ? never : T
+type OnlyError<T> = [T] extends [Error] ? Error : never
+
 
 const INFO_STYLE: string = 'color: #38a169; font-weight: bold;'; // 绿色样式
+const DANGER_STYLE: string = 'color: red; font-weight: bold;'; // 红色样式
+
 
 class Log {
-    info<T>(title: NoErrorType<T>): undefined {
-        console.log(`%c${title}`, INFO_STYLE, );
+    info<T>(content: NoErrorType<T>): undefined {
+        console.log(`%c${content}`, INFO_STYLE, );
+    }
+    danger<T>(error:OnlyError<T>){
+        console.log(`%c${error}`, DANGER_STYLE, );
     }
 }
 export default Log;
