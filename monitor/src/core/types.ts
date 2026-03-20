3e517cb1363fe26e0ade91187c6a5011766d40ee
宋金涛 <mr.songjintao@gmail.com>
2026-03-20 17:31:25 +0800

    feat:重构

--- monitor/src/core/types.ts
+++ monitor/src/core/types.ts
@@ -1,18 +1,34 @@
+export interface TrackerInstance {
+  capture(payload: Partial<ErrorPayload>): void;
+  captureException(error: Error, extraInfo?: Record<string, any>): void;
+  setUser(user: Record<string, any>): void;
+  setTag(key: string, value: string): void;
+}
+
 export interface Integration {
   name: string;
-  setup: (tracker: ErrorTracker) => void;
+  setup: (tracker: TrackerInstance) => void; 
 }
 
 export interface SDKOptions {
-  dsn: string;
-  appVersion?: string;
+  dsn: string; // 后端接收地址
+  appVersion?: string; // 应用版本，方便排查是哪个版本的 Bug
   environment?: "development" | "production" | "test";
+  // 拦截器
   beforeSend?: (event: ErrorPayload) => ErrorPayload | null;
-  // 【新增】允许用户传入集成数组
-  integrations?: Integration[]; 
 }
 
-
-export type ErrorPayload{
-    
+export interface ErrorPayload {
+  type: string;
+  message: string;
+  stack?: string | null;
+  time: number;
+  url: string;
+  // 静态配置
+  appVersion?: string;
+  environment?: string;
+  // 动态上下文
+  user?: Record<string, any>;
+  tags?: Record<string, string>;
+  extra?: Record<string, any>; // 留给开发者塞任意数据的黑洞
 }
