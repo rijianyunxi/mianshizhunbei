2b2611f5e00582a9825381f2af1b2ac44db77522
宋金涛 <mr.songjintao@gmail.com>
2026-04-22 12:27:08 +0800

    1

--- ai/react/vite.config.ts
+++ ai/react/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import lifecycleLoggerPlugin from './plugin/index.ts'
 // import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
